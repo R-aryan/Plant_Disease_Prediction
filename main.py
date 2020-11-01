a7de013217f8dd5952feef23ed0ef34fb64794d1
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-11-01 19:44:15 +0530

    updated files

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
 import tensorflow as tf
 
 # print(tf.test.is_gpu_available)
-print(tf.__version__)
+# print(tf.__version__)
 
--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
 class Config:
     IMAGE_SIZE = [224, 224, 3]
     EPOCHS = 30
+    MODEL_PATH = 'models/inception_model.h5'
