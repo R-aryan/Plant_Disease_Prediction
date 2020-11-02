e78b5bf19e1c545ed50b333d4add69c89ddee967
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-11-02 19:28:15 +0530

    added code for inference

--- src/inference/app.py
+++ src/inference/app.py
@@ -8,6 +8,31 @@ import re
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
 
+# prediction utils
+from src.inference.predict import ModelPrediction
+
 # Define a flask app
 app = Flask(__name__)
 
+# create model prediction instance
+model = ModelPrediction()
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        base_path = os.path.dirname(__file__)
+
+        file_path = os.path.join(
+            base_path, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
