b6254518386d315a095114305c3873c475b629ff
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2020-11-02 19:50:27 +0530

    removed corrupted model weights

--- src/config.py
+++ src/config.py
@@ -2,5 +2,5 @@
 class Settings:
     TARGET_SIZE = (224,224)
     EPOCHS = 30
-    MODEL_PATH_INCEPTION = 'models/inception_model.h5'
-    MODEL_PATH_VGG = 'models/vgg16_model.h5'
+    MODEL_PATH_INCEPTION = '/models/inception_model.h5'
+    MODEL_PATH_VGG = '/models/vgg16_model.h5'
